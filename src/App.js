aaa5bb8eed0d54aca2a9a780c81059d33e3c33ae
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-11-17 17:32:50 +0100

    add new components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Main from './components/Main';
 import Aside from './components/Aside';
 import Clients from './components/Clients';
+import New from './components/New';
+import Edit from './components/Edit';
+import { useState } from 'react';
 
 
 function App() {
-  return (
+
+  const [showInvoice, setShowInvoice] = useState(false);
+
+  return ( 
     <div className="App">
       <Aside></Aside>
       <Main></Main>
+      <New></New>
+      <Edit></Edit>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+
 export default function Main(props) {
+
+    const { showInvoice, invoice } = props; 
+
     return (
     <main>
 
@@ -9,7 +13,7 @@ export default function Main(props) {
             <div className="headline">
                 <div className="headline-left">
                     <h1 className="invoices">Invoices</h1>
-                    <p>There are <span> *7* </span> total invoices</p>
+                    <p>No invoices</p>
                 </div>
             </div>
 
@@ -20,82 +24,30 @@ export default function Main(props) {
             
         </header>
 
+
         <section>
 
-            <div className="item">
-                <div className="invoice-number"><span>#</span></div>
-                <div className="invoice-date"></div>
-                <div className="invoice-name"></div>
-                <div className="invoice-price"><span className="pound">$ </span></div>
-                <div className="invoice-status">
-                    <div className="circle"></div>
-                    <p className="letter"></p>
-                </div>
-                <img className="arrow right" src={require('../assets/icon-arrow-right.svg').default} alt="arrow-right"/>
-            </div>
-          
+                {showInvoice ?
+                    <div key={invoice.id} className="item"> 
+                        <div className="invoice-number"><span>#</span>{invoice.number}</div>
+                        <div className="invoice-date">{invoice.date}</div>
+                        <div className="invoice-name">{invoice.name}</div>
+                        <div className="invoice-price"><span className="pound">$ </span>{invoice.price}</div>
+                        <div className="invoice-status">
+                            <div className="circle"></div>
+                            <p className="letter">{invoice.status}</p>
+                        </div>
+                        <img className="arrow right" src={require('../assets/icon-arrow-right.svg').default} alt="arrow-right"/>
+                    </div> : (
+                        <div className="empty-page"> 
+                        <img src={require('../assets/illustration-empty.svg').default} alt="empty-page"/>
+                        <h3>There is nothing here</h3>
+                        <p className="empty-page-text">Create an invoice by clicking the <span className="bold">New Invoice</span> button and get started</p>
+                    </div>
+                    )    
+                }
+
         </section>
-
-
-{/*           
-              Filter by status
-              Draft
-              Pending
-              Paid
-              
-              New Invoice
-
-              <!-- No invoices -->
-              There is nothing here
-              Create an invoice by clicking the New Invoice button and get started
-              <!-- No invoices end -->
-
-              <!-- Create new invoice form -->
-              Go back
-
-              New Invoice
-              
-              Bill From
-              
-              Street Address
-              City
-              Post Code
-              Country
-              
-              Bill To
-              
-              Client's Name
-              Client's Email
-              e.g. email@example.com
-              Street Address
-              City
-              Post Code
-              Country
-              
-              Invoice Date
-              
-              Payment Terms
-              Net 1 day
-              Net 7 days
-              Net 14 days
-              Net 30 days
-              
-              Project Description
-              e.g. Graphic Design Service
-              
-              Item List
-              
-              Item Name
-              Qty.
-              Price
-              Total
-              
-              Add New Item
-              
-              Discard
-              Save as Draft
-              Save & Send
-              <!-- Create new invoice end --> */}
 
     </main>
     );
